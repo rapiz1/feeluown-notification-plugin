e0936865233d78a12cfdeb8fa8af3707555eabe4
Rapiz <rapiz@foxmail.com>
2020-02-13 15:21:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# FeelUOwn Desktop Notification Plugin
+为 FeelUOwn 正在播放的歌曲提供桌面通知
+
+![screenshot](screenshot.png)
+## Dependency
+- FeelUOwn
+- python-gobject
+## Installation
+https://github.com/feeluown/FeelUOwn/issues/148
---
+++ __init__.py
@@ -0,0 +1,43 @@
+from fuocore.models import reverse
+from fuocore import aio
+from feeluown.fuoexec import add_hook, rm_hook
+from gi.repository import Notify, GdkPixbuf
+
+__alias__ = 'Desktop Notification'
+__version__ = '0.0.1'
+__desc__ = 'Show the metadata(song, artists, cover) of songs by desktop notifications'
+
+notifier = None 
+
+class Notifier:
+  def __init__(self, app):
+    self._app = app
+    self._notification = Notify.Notification.new('','','')
+    Notify.init("FeelUOwn")
+
+  async def popup(self, song):
+    if song is None:
+      return
+    title = song.title_display
+    artists_name = song.artists_name_display
+    album = song.album
+
+    data = await self._app.img_mgr.get(album.cover, reverse(album, '/cover'))
+    loader = GdkPixbuf.PixbufLoader.new()
+    loader.write(data)
+    loader.close()
+    gdk_image = loader.get_pixbuf()
+    self._notification.update(title, artists_name)
+    self._notification.set_image_from_pixbuf(gdk_image)
+    self._notification.show()
+
+  def caller(self, song):
+    aio.create_task(self.popup(song))
+
+def enable(app):
+  notifier = Notifier(app)
+  add_hook('app.player.playlist.song_changed', notifier.caller)
+
+def disable(app):
+  notifier = None
+  rm_hook('app.player.playlist.song_changed', notifier.caller)
